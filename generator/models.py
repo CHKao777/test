7e5fd620e0209b82a6624df92bb31451e6b394ae
CHKao <83213131+CHKao777@users.noreply.github.com>
2022-03-14 23:23:22 +0800

    Add files via upload

---
+++ generator/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+# class FeedBack(models.Model):
+#     #citation_text
+#     citing_context = models.TextField()
+#     cited_context = models.TextField()
+#     citation_text = models.TextField()
+#     citation_text_quality = models.IntegerField()
+#     citation_intent_quality = models.IntegerField()
+#     comments = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
